1822d5e314dc6be06984249190d4d50a22f6d094
Stian Fredrikstad <stian.fredrikstad@bekk.no>
2015-02-18 21:13:02 +0100

    Bekk brand and outline

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'OWASP Top 10',
+    subtitle: '',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Stian Fredrikstad',
+    company: '',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: ''
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
