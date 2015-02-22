ccdd6d7b8e2005a049efc184622de24547cafbc0
Stian Fredrikstad <stian.fredrikstad@bekk.no>
2015-02-22 16:41:07 +0100

    A2 og A3

--- demo/app/controllers/application_controller.rb
+++ demo/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_headers
+
+  def set_headers
+    response.headers["X-XSS-Protection"] = "0; mode=block"
+  end
 end
